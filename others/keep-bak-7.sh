699277b4405bd5b8cd5be6594ba3db0e15ccf640
tangchengwei <tangchengwei@mftour.cn>
2017-04-19 15:50:34 +0800

    change 2016 2017

--- others/keep-bak-7.sh
+++ others/keep-bak-7.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-n=`ls -l /data/backup | grep 2016.*[0-9]$ | wc -l`
+n=`ls -l /data/backup | grep 2017.*[0-9]$ | wc -l`
 if [ "$n" -gt 7 ];then
    echo `hostname`
-   num=`ls -l /data/backup/ | grep 2016.*[0-9]$ | wc -l`
+   num=`ls -l /data/backup/ | grep 2017.*[0-9]$ | wc -l`
    let num1=$num-7
-   #ls -l /data/backup/ | grep 2016.*[0-9]$ | head -$num1
-   cd /data/backup && rm -rf `ls /data/backup/ | grep 2016.*[0-9]$ | head -$num1`
+   #ls -l /data/backup/ | grep 2017.*[0-9]$ | head -$num1
+   cd /data/backup && rm -rf `ls /data/backup/ | grep 2017.*[0-9]$ | head -$num1`
 fi
